7f59f2ce3db91e8182838191b5ae38668caaad03
Aniket Nishad <nishadaniket79@gmail.com>
2025-02-22 09:22:58 +0000

    creating 02

---
+++ tutorials/02.js
@@ -0,0 +1,17 @@
+// Operations
+
+// console.log(2+"4"+8);
+// console.log(2+9+"9");
+
+// console.log(+true);
+
+console.log(null>0);//here null is converted into number and treated as 0
+console.log(null==0);// here null is compared value vise without any conversion
+console.log(null>=0);// here to null is converted into number and treated as 0
+
+//in comparison the null is converted in number as 0 while in == case its value is being compared without 
+// converted into number
+
+
+
+
