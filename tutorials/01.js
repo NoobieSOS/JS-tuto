8835ef38f4cfd2bed2c72bbdb22edb5fc21ba48a
Aniket Nishad <nishadaniket79@gmail.com>
2025-02-22 08:26:08 +0000

    conversion completed

--- tutorials/01.js
+++ tutorials/01.js
@@ -1,8 +1,24 @@
-let score= null
+// let score= null
 
-console.log(typeof (score));
-console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let number = 1;
+let inBoolean= Boolean(number);
+console.log(inBoolean);
+
+let number1 = 0;
+let inBoolean1 = Boolean(number1);
+console.log(inBoolean1);
+
+let string =""
+let inBoolean2 = Boolean(string);
+console.log(inBoolean2);
+
+let string1= "Aniket"
+let inBoolean3= Boolean(string1);
+console.log(inBoolean3);
